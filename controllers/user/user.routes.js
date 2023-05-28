a024e385b88dc8d3f1fe1c2708da89e6a3e23681
Julia Barber <giulia@tubesoft.cl>
2023-05-28 14:51:45 -0300

    github actions fix

--- controllers/user/user.routes.js
+++ controllers/user/user.routes.js
@@ -21,38 +21,32 @@ const {
 } = require('./user.validations');
 
 const { isAuthorized } = require('../auth/auth.middlewares');
-const {
-  READ, WRITE, UPDATE, REMOVE,
-} = require('../../misc/const/permissions');
-
-const { verifyGroup } = require('../../misc/utils/group');
 
 const controllerName = 'user';
 
 module.exports = (app, router) => {
-  verifyGroup(app, controllerName);
   router.post(
     '/users/create',
-    isAuthorized(app, WRITE, controllerName),
+    isAuthorized(app, controllerName),
     postCreateValidation(app),
     postCreateUser(app),
   );
   router.get(
     '/users',
-    isAuthorized(app, READ, controllerName),
+    isAuthorized(app, controllerName),
     getUsersPerPageValidation(app),
     getUsersPerPage(app),
   );
   router.get(
     '/all/users',
-    isAuthorized(app, READ, controllerName),
+    isAuthorized(app, controllerName),
     getAllUsers(app),
   );
   router.get('/users/active', getUsersPerPageValidation(app), getActiveUsers(app));
-  router.get('/user', isAuthorized(app, READ, controllerName), getCurrentUser(app));
+  router.get('/user', isAuthorized(app, controllerName), getCurrentUser(app));
   router.get('/users/filter', getFilteredUsersValidation(app), getFilteredUsers(app));
   router.get('/users/:id', getUserByIdValidation(app), getUserById(app));
-  router.delete('/user/:id', isAuthorized(app, REMOVE, controllerName), deleteUser(app));
-  router.put('/user/edit', isAuthorized(app, UPDATE, controllerName), putEditUserValidation(app), putEditUser(app));
+  router.delete('/user/:id', isAuthorized(app, controllerName), deleteUser(app));
+  router.put('/user/edit', isAuthorized(app, controllerName), putEditUserValidation(app), putEditUser(app));
   router.get('/users/role/:roleId', getUsersByRoleValidation(app), getUsersByRole(app));
 };
