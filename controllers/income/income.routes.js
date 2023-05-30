8f19fb779f00f05dd10e03fed15cba9c99c48f1d
Nahuel Paolinelli <nahuel.paolinelli@gmail.com>
2023-05-30 17:49:02 -0300

    CRUD de incomes terminado

---
+++ controllers/income/income.routes.js
@@ -0,0 +1,53 @@
+const { isAuthorized } = require('../auth/auth.middlewares');
+const {
+  postIncome,
+  putIncome,
+  deleteIncome,
+  getIncomeById,
+  getIncomePerPage,
+} = require('./income.ctrl');
+
+const {
+  postIncomeValidation,
+  putIncomeValidation,
+  deleteIncomeValidation,
+  getIncomeByIdValidation,
+  getIncomesPerPageValidation,
+} = require('./income.validations');
+
+module.exports = (app, router) => {
+  router.post(
+    '/income/create',
+    isAuthorized(),
+    postIncomeValidation(app),
+    postIncome(app),
+  );
+
+  router.put(
+    '/income/edit',
+    isAuthorized(),
+    putIncomeValidation(app),
+    putIncome(app),
+  );
+
+  router.put(
+    '/income/delete',
+    isAuthorized(),
+    deleteIncomeValidation(app),
+    deleteIncome(app),
+  );
+
+  router.get(
+    '/income/:id',
+    isAuthorized(),
+    getIncomeByIdValidation(app),
+    getIncomeById(app),
+  );
+
+  router.get(
+    'income',
+    isAuthorized(),
+    getIncomesPerPageValidation(app),
+    getIncomePerPage(app),
+  );
+};
---
+++ controllers/income/index.js
@@ -0,0 +1,5 @@
+const routes = require('./income.routes');
+
+module.exports = (app, router) => {
+  routes(app, router);
+};
