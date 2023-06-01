96d902a979942c97682a9d819de30eb09032aefa
Nahuel Paolinelli <nahuel.paolinelli@gmail.com>
2023-05-31 22:13:31 -0300

    arreglo en index de rutas

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const auth = require('../controllers/auth');
 const user = require('../controllers/user');
+const income = require('../controllers/income');
 
 module.exports = (app) => {
   const router = express.Router();
 
-  auth(app, router);
   user(app, router);
+  income(app, router);
 
   return router;
 };
